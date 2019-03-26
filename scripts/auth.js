865050a2f9edc7129aceeafda1e57438855c7b77
janarf <janarf@hotmail.com>
2019-03-26 20:28:10 -0300

    autenticaçao de cadastro e login ok

---
+++ scripts/auth.js
@@ -0,0 +1,33 @@
+$(document).ready(() => {
+
+  $('#sign-up-btn').on('click', (e) => {
+    e.preventDefault();
+    let name = $('#input-name').val();
+    let email = $('#input-email').val();
+    let password = $('#input-password').val();
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((response) => {
+        response.user.updateProfile({ displayName: name });
+        console.log(response.user)
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+  });
+
+
+  $('#sign-in-btn').on('click', (e) => {
+    e.preventDefault();
+    let email = $('#login-email').val();
+    let password = $('#login-password').val();
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((response) => {
+        console.log(response.user)
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+  });
+});
---
+++ scripts/config.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyD7Bdj2Jbfym1PvVtfqtEEp5iAEg6P-hA0",
+  authDomain: "redesocial-18fde.firebaseapp.com",
+  databaseURL: "https://redesocial-18fde.firebaseio.com",
+  projectId: "redesocial-18fde",
+  storageBucket: "redesocial-18fde.appspot.com",
+  messagingSenderId: "533368476703"
+};
+firebase.initializeApp(config);
