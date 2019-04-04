28eca75114f01a24c3dcc7e3b05c7a0b3224586f
Boo <almeidaribeiro.b@gmail.com>
2019-04-04 13:28:19 -0300

    post privado e user id posts

--- scripts/post.js
+++ scripts/post.js
@@ -2,8 +2,7 @@ function post(text, database, USER_ID) {
   database.ref('posts/' + USER_ID).push({
     post: text,
     likeCount: 0,
-    user: USER_ID
-    //preciso desse id aqui?
+    privado: false 
   })
 
 }
