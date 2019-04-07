eaf013bd8a4aa0761b6212a863ac2bc4bb643887
alixlab <alicelabarca@gmail.com>
2019-04-07 19:21:10 -0300

    Criadas as funções para adicionar e remover amigos

--- scripts/friends.js
+++ scripts/friends.js
@@ -11,24 +11,59 @@ $(document).ready(() => {
             const users = snapshot.val();
             snapshot.forEach(function (childSnapshot) {
                 const key = childSnapshot.key;
-                suggestion(users[key].username, key)
+                suggestion(users[key].username, key);
+                friendList(users[key].username, key);
             });
         })
 });
 
 function suggestion(name, key) {
-    if (key !== USER_ID) {
-        $("#suggestion-list").append(`
-        <li>
-            <span>${name}</span>
-            <button data-friend-id=${key}>Seguir</button>
-        </li>
-        `)
-    }
-    $(`button[data-friend-id=${key}]`).click(function() {
-        database.ref("friendship/" + USER_ID).push({
-            friendId: key
-        })
-    })
+    database.ref("friendship/" + USER_ID).orderByChild("friendId").equalTo(key).once("value", snapshot => {
+        if (!snapshot.exists()) {
+            if (key !== USER_ID) {
+                $("#suggestion-list").append(`  
+                <li>
+                    <span>${name}</span>
+                    <button data-friend-id=${key}>Seguir</button>
+                </li>
+                `)
+            }
+            $(`button[data-friend-id=${key}]`).click(function () {
+                database.ref("friendship/" + USER_ID).push({
+                    friendId: key
+                })
+                friendList(name, key);
+                $(this).parent().remove();
+            })
+        }
+    });
 }
 
+function friendList(name, key) {
+    database.ref("friendship/" + USER_ID).orderByChild("friendId").equalTo(key).once("value", snapshot => {
+        if (snapshot.exists()) {
+            if (key !== USER_ID) {
+                $("#friend-list").append(`  
+                <li>
+                    <span>${name}</span>
+                    <button data-friend-id=${key}>Remover</button>
+                </li>
+                `)
+            }
+            $(`button[data-friend-id=${key}]`).click(function () {
+                database.ref("friendship/" + USER_ID).once("value")
+                    .then(function (snapshot) {
+                        const temp = snapshot.val();
+                        snapshot.forEach(function (childSnapshot) {
+                            const friendKey = childSnapshot.key;
+                            if (key == temp[friendKey].friendId) {
+                                database.ref(`friendship/${USER_ID}/${friendKey}`).remove();
+                            }
+                        });
+                    })
+                $(this).parent().remove();
+                suggestion(name, key);
+            })
+        }
+    });
+}
--- scripts/timeline.js
+++ scripts/timeline.js
@@ -54,7 +54,8 @@ function post(text, database, USER_ID, private = false) {
   database.ref('posts/' + USER_ID).push({
     post: text,
     likeCount: 0,
-    privado: private
+    privado: private,
+    timestamp: date
   })
 }
 
