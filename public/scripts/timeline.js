b6f5e4b20894a679e50a45ef7b1971bfa709cd40
janarf <janarf@hotmail.com>
2019-04-11 00:06:46 -0300

    bugs de auth resolvidos, carregando foto com google auth

--- public/scripts/timeline.js
+++ public/scripts/timeline.js
@@ -19,30 +19,28 @@ $(document).ready(function() {
     setPublicOrPrivateTimeline($('.select-public-private-timeline'))
       .once('value')
       .then((snapshot) => {
-        const posts = snapshot.val()
+        const posts = snapshot.val();
         if (!posts) return;
-        $(".post-list").html("")
+        $(".post-list").html("");
         Object.keys(posts).forEach(key => {
           database.ref("users/" + USER_ID)
             .once('value')
             .then(function(snapshot) {
               const name = snapshot.val().username;
               const imgURL = snapshot.val().imgURL;
-              $(".post-list").append(templateStringPost(posts[key].post, name, key, posts[key].likeCount, imgURL))
-
-              setKeyToLike(key)
-              setKeyToEdit(key)
-              setKeyToDelete(key)
-              setKeyToComment(key)
-
+              $(".post-list").append(templateStringPost(posts[key].post, name, key, posts[key].likeCount, imgURL));
+              setKeyToLike(key);
+              setKeyToEdit(key);
+              setKeyToDelete(key);
+              setKeyToComment(key);
             })
         })
       })
   }
 
   function postInput() {
-    $(".post-input").click(function(event) {
-      $(".post-input").val("")
+    $(".post-input").click(() => {
+      $(".post-input").val("");
     })
   }
 
@@ -50,17 +48,16 @@ $(document).ready(function() {
     event.preventDefault();
     let text = $(".post-input").val();
     if (text === "") {
-      $(".post-text-btn").on(function() {
+      $(".post-text-btn").on(() => {
         $(this).prop("disabled", true);
       });
     } else {
-      $(".post-list").html("")
-
+      $(".post-list").html("");
       post(text, database, USER_ID, setPublicOrPrivatePost($(".select-public-private")));
-      loadTimeline()
+      loadTimeline();
       $(".post-input").val('Pegue seu biscoito');
-      postInput()
-      $(".select-public-private").val("public")
+      postInput();
+      $(".select-public-private").val("public");
     };
   });
 
@@ -82,113 +79,80 @@ $(document).ready(function() {
       </div>`
     } else {
       content = `<p data-text-id="${key}" id="text-post-${key}">${text}</p>`
-
     }
     return `
-
   <div data-div=${key} class="container mt-4 p-4 bg-light">
     <div class="container">
       <div class="row">
         <div class="col-2 col-md-1 m-0 p-0">
-
-
-    
           <figure class="background--gray rounded-circle profile-picture">
-          <img class="w-100 rounded-circle margin-0  profile-picture" src="${imgURL}" alt="">
+            <img class="w-100 rounded-circle margin-0  profile-picture" src="${imgURL}" alt="">
           </figure>
         </div>
         <div class="col-9 col-md-10 text--gray text--big">
-
         <div class="text-right mr-n5">
-        <input data-edit="${key}" type="image"  id="edit-button-${key}" src="../img/icons/pencil-edit-button.png" placeholder="Editar" height=20 weigth=20>&nbsp;&nbsp
-        <input data-key="${key}" type="image" id="delete-button-${key}"  data-toggle="modal" data-target="#exampleModalCenter" src="../img/icons/rubbish-bin.png" placeholder="Excluir" height=20 weigth=20>&nbsp;&nbsp
+          <input data-edit="${key}" type="image"  id="edit-button-${key}" src="../img/icons/pencil-edit-button.png" placeholder="Editar" height=20 weigth=20>&nbsp;&nbsp
+          <input data-key="${key}" type="image" id="delete-button-${key}"  data-toggle="modal" data-target="#exampleModalCenter" src="../img/icons/rubbish-bin.png" placeholder="Excluir" height=20 weigth=20>&nbsp;&nbsp
         </div>
-        
           <p><strong>${name}</strong></p>
-
-
-        
-
-        ${content}
-     
+          ${content}
         <input data-text-input="${key}" class="d-none" value=${text} id="edit-${key}">
-
-        
         </div>
       </div>
     </div>
-
-
-   
     <div>
-    <input type="image" data-like=${key} value=${likeCount} src="../img/cookie.ico" height=25 weight=25>&nbsp<span>${likeCount}</span>&nbsp;&nbsp
-    <input data-comment-btn="${key}" type="image" value=${comment} src="../img/icons/balloongreen.png" height=25 weigth= 25>&nbsp;&nbsp
-    <button type="button" data-save="${key}" class="d-none btn-xs border-0 btn--green font-weight-bold rounded text-white" id="save-button-${key}" > Salvar </button>
-
-
-  <hr>
-  <div>
-    <p class="text--gray"><strong>Comentários</strong></p>
-    <div class="comment-list" data-area=${key}></div>
-  </div>
-
+      <input type="image" data-like=${key} value=${likeCount} src="../img/cookie.ico" height=25 weight=25>&nbsp<span>${likeCount}</span>&nbsp;&nbsp
+      <input data-comment-btn="${key}" type="image" value=${comment} src="../img/icons/balloongreen.png" height=25 weigth= 25>&nbsp;&nbsp
+      <button type="button" data-save="${key}" class="d-none btn-xs border-0 btn--green font-weight-bold rounded text-white" id="save-button-${key}" > Salvar </button>
+    </div>
+    <hr>
+    <div>
+      <p class="text--gray"><strong>Comentários</strong></p>
+      <div class="comment-list" data-area=${key}></div>
+    </div>
 </div>`
+  }
 
-}
+  function setKeyToEdit(key) {
+    $(`input[data-edit=${key}]`).click(function() {
+      $(`#edit-${key}`).removeClass("d-none");
+      $(`#text-post-${key}`).addClass("d-none");
+      $(`#save-button-${key}`).removeClass("d-none");
+      $(`#delete-button-${key}`).addClass("d-none");
+      $(`#edit-button-${key}`).addClass("d-none");
 
-function setKeyToEdit(key) {
-  $(`input[data-edit=${key}]`).click(function (){
-    $(`#edit-${key}`).removeClass("d-none");
-    $(`#text-post-${key}`).addClass("d-none");
-    $(`#save-button-${key}`).removeClass("d-none");
-    $(`#delete-button-${key}`).addClass("d-none");
-    $(`#edit-button-${key}`).addClass("d-none");
+      // document.getElementById(`edit-${key}`).className = "";
+      // document.getElementById(`text-post-${key}`).className = "edit-hidden";
+      // document.getElementById(`save-button-${key}`).className = "";
+      // document.getElementById(`delete-button-${key}`).className = "edit-hidden";
+      // document.getElementById(`edit-button-${key}`).className = "edit-hidden";
 
-    // document.getElementById(`edit-${key}`).className = "";
-    // document.getElementById(`text-post-${key}`).className = "edit-hidden";
-    // document.getElementById(`save-button-${key}`).className = "";
-    // document.getElementById(`delete-button-${key}`).className = "edit-hidden";
-    // document.getElementById(`edit-button-${key}`).className = "edit-hidden";
-   
-  })
-  $(`button[data-save=${key}]`).click(function (){
-    let newText = document.getElementById(`edit-${key}`).value;
-    document.getElementById(`text-post-${key}`).innerHTML = newText;
+    })
+    $(`button[data-save=${key}]`).click(function() {
+      let newText = document.getElementById(`edit-${key}`).value;
+      document.getElementById(`text-post-${key}`).innerHTML = newText;
 
-    $(`#edit-${key}`).addClass("d-none");
-    $(`#text-post-${key}`).removeClass("d-none");
-    $(`#save-button-${key}`).addClass("d-none");
-    $(`#delete-button-${key}`).removeClass("d-none");
-    $(`#edit-button-${key}`).removeClass("d-none");
-
-
-    // document.getElementById(`edit-${key}`).className = "edit-hidden";
-    // document.getElementById(`text-post-${key}`).className = "";
-    // document.getElementById(`save-button-${key}`).className = "edit-hidden";
-    // document.getElementById(`delete-button-${key}`).className = "";
-    // document.getElementById(`edit-button-${key}`).className = "";
-
-    database.ref(`posts/${USER_ID}/${key}`).update({
-      post: newText
-    });   
-  })
-  
-}
+      $(`#edit-${key}`).addClass("d-none");
+      $(`#text-post-${key}`).removeClass("d-none");
+      $(`#save-button-${key}`).addClass("d-none");
+      $(`#delete-button-${key}`).removeClass("d-none");
+      $(`#edit-button-${key}`).removeClass("d-none");
 
 
-function setKeyToDelete(key) {
-  $(`input[data-key=${key}]`).click(function () {
-    let deletePost = document.getElementById("delete-modal");
-    
-    if(deletePost){
-      document.getElementById("delete-modal").addEventListener("click", function(){
-        $(`[data-div=${key}]`).remove();
-        $(".post-input").val("");
-  
-        database.ref(`posts/${USER_ID}/${key}`).remove();
-        database.ref(`comments/${key}`).remove();
-        $("#exampleModalCenter").modal('hide');
-      })
+      // document.getElementById(`edit-${key}`).className = "edit-hidden";
+      // document.getElementById(`text-post-${key}`).className = "";
+      // document.getElementById(`save-button-${key}`).className = "edit-hidden";
+      // document.getElementById(`delete-button-${key}`).className = "";
+      // document.getElementById(`edit-button-${key}`).className = "";
+
+      database.ref(`posts/${USER_ID}/${key}`).update({
+        post: newText
+      });
+    })
+
+  }
+
+
 
 
   function setKeyToEdit(key) {
@@ -214,8 +178,8 @@ function setKeyToDelete(key) {
         post: newText
       });
     })
+  }
 
-  
 
 
   function setKeyToDelete(key) {
@@ -426,4 +390,4 @@ function setKeyToDelete(key) {
           })
       });
   }
-});
+})
